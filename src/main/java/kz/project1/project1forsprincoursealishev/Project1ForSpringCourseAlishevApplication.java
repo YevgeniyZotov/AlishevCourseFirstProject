21b3916dccc4ea451c628ac16e0ce943a606b15e
Yevgeniy Zotov <jenya12345zotov@gmail.com>
2025-01-09 00:00:02 +0500

    starter pack

---
+++ src/main/java/kz/project1/project1forsprincoursealishev/Project1ForSpringCourseAlishevApplication.java
@@ -0,0 +1,12 @@
+package kz.project1.project1forsprincoursealishev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1ForSpringCourseAlishevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project1ForSpringCourseAlishevApplication.class, args);
+    }
+}
---
+++ src/test/java/kz/project1/project1forsprincoursealishev/Project1ForSpringCourseAlishevApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.project1.project1forsprincoursealishev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ForSpringCourseAlishevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
