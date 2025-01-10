06dd3a1100f47763731f5d5ebad2b888b0cea0bd
Yevgeniy Zotov <jenya12345zotov@gmail.com>
2025-01-10 23:14:44 +0500

    add PersonValidator

---
+++ src/main/java/kz/project1/project1forsprincoursealishev/validators/PersonValidator.java
@@ -0,0 +1,30 @@
+package kz.project1.project1forsprincoursealishev.validators;
+
+import kz.project1.project1forsprincoursealishev.models.Person;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.time.LocalDate;
+
+public class PersonValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+
+        // Проверка на fullName
+        if (person.getFullName() == null || person.getFullName().trim().isEmpty() || person.getFullName().length() < 2 || person.getFullName().length() > 100) {
+            errors.rejectValue("fullName", "person.fullName.invalid", "Name should be between 2 and 100 characters");
+        }
+
+        // Проверка года рождения
+        int currentYear = LocalDate.now().getYear();
+        if (person.getId() < 1900 || person.getBirthYear() > currentYear) {
+            errors.rejectValue("birthYear", "person.birthYear.invalid", "Birth year should be between 1900 and the current year");
+        }
+    }
+}
